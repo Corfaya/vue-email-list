bd4b37f87435a06fa13b498f0af067bb353e9c3a
Corfaya <lauracorrado0696@gmail.com>
2024-07-25 15:33:15 +0200

    fix empty array

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -8,6 +8,7 @@ createApp({
     },
     methods: {
         getEmails() {
+            this.emails = []
             for(let i = 0; i<10; i++) {
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
                     this.emails.push(resp.data.response)
